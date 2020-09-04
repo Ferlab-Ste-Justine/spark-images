2aad72f34ca10f6b6d86eb7347fe5da77c572f10
jecos <jcostanza@ferlab.bio>
2020-09-04 16:01:34 -0400

    fix : Ferlab-Ste-Justine/clin-project#438 Add jars required for using ceph

--- build_spark_3.0.sh
+++ build_spark_3.0.sh
@@ -4,4 +4,7 @@ mkdir -p work
 cd work
 wget http://apache.mirror.iweb.ca/spark/spark-3.0.0/spark-3.0.0-bin-hadoop3.2.tgz
 tar xf spark-3.0.0-bin-hadoop3.2.tgz
+wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.855/aws-java-sdk-bundle-1.11.855.jar -o spark-3.0.0-bin-hadoop3.2/jars/aws-java-sdk-bundle-1.11.855.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.2.1/hadoop-aws-3.2.1.jar -o spark-3.0.0-bin-hadoop3.2/jars/hadoop-aws-3.2.1.jar
+
 spark-3.0.0-bin-hadoop3.2/bin/docker-image-tool.sh -r ferlab -t 3.0.0 build
