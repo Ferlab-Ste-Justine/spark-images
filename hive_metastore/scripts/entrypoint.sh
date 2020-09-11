dead7320bbe396d3ca2a36c15263dffea71a808d
Eric Vallee <eric_vallee@webificservices.com>
2020-09-11 13:00:04 -0400

    fix: Ferlab-Ste-Justine/clin-project#440 Previously forgot to add db host to parametrizable inputs

--- hive_metastore/scripts/entrypoint.sh
+++ hive_metastore/scripts/entrypoint.sh
@@ -4,7 +4,7 @@ export HADOOP_HOME=/opt/hadoop-3.2.0
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
 export JAVA_HOME=/usr/local/openjdk-8
 
-envsubst '${DB} ${DB_USER} ${DB_PASSWORD}' < ${HIVE_HOME}/conf/metastore-site.xml > ${HIVE_HOME}/conf/metastore-site.xml
+envsubst '${DB_HOST} ${DB} ${DB_USER} ${DB_PASSWORD}' < ${HIVE_HOME}/conf/metastore-site.xml > ${HIVE_HOME}/conf/metastore-site.xml
 
 /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType postgres
 /opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
