a0ce9a10b8571abeb3fe3d9e169f202b9475afa7
Eric Vallee <eric_vallee@webificservices.com>
2020-09-17 18:16:41 -0400

    feat: Ferlab-Ste-Justine/clin-project#446 Change spark executor pod name to be based on the client pod name for easier visual association when viewing pods

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,12 @@ RUN mkdir -p /opt/spark/conf && touch /opt/spark/conf/spark-defaults.conf && \
     touch /opt/spark/conf/spark-defaults.conf && \
     chown $SPARK_USER:$SPARK_USER /opt/spark/conf/spark-defaults.conf
 
+RUN mkdir -p /opt/spark-configs/auto
+COPY auto.conf /opt/spark-configs/auto/auto.conf
+RUN chown -R $SPARK_USER:$SPARK_USER /opt/spark-configs
+
+RUN apt-get update && apt-get install -y gettext
+
 USER $SPARK_USER
 
 COPY client-entrypoint.sh /opt/client-entrypoint.sh
--- client-entrypoint.sh
+++ client-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "$SPARK_CLIENT_POD_NAME" ]; then
+    echo "SPARK_CLIENT_POD_NAME environment variable needs to be defined";
+    exit 1;
+fi
+
 if [ ! -f "/opt/spark/conf/spark-defaults.conf" ]; then
     echo "/opt/spark/conf/spark-defaults.conf does not exist";
     exit 1;
@@ -9,6 +14,8 @@ if [ -z "$SPARK_JAR" ]; then
     echo "SPARK_JAR environment variable needs to be defined";
     exit 1;
 fi
+
+envsubst '${SPARK_CLIENT_POD_NAME}' < /opt/spark-configs/auto/auto.conf > /opt/spark-configs/auto/auto.conf
 
 for CONFIG_FILE in /opt/spark-configs/*/*; do
     cat $CONFIG_FILE >> /opt/spark/conf/spark-defaults.conf;
