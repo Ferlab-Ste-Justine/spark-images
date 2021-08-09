ccf8f0e2595049e169199ddfd98f4e298ed1b6cf
cbotek <botekchristophe@gmail.com>
2021-08-09 14:14:25 -0400

    feat(CLIN): #192 Build spark 3.1.2 with hadoop 3.3.1

--- build_spark.sh
+++ build_spark.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
-export SPARK_VERSION=${SPARK_VERSION:-3.0.1}
+export SPARK_VERSION=${SPARK_VERSION:-3.1.2}
+export HADOOP_VERSION=${HADOOP_VERSION:-3.3.1}
 export SPARK_IMAGE_TAG=${SPARK_IMAGE_TAG:-$SPARK_VERSION}
 
-if [ ! -d "./work/spark-${SPARK_VERSION}-bin-hadoop3.2" ]; then
-  mkdir -p work
-  (
-      cd work;
-      wget http://apache.mirror.iweb.ca/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop3.2.tgz;
-      tar xf spark-${SPARK_VERSION}-bin-hadoop3.2.tgz;
-      wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.375/aws-java-sdk-bundle-1.11.375.jar -O spark-${SPARK_VERSION}-bin-hadoop3.2/jars/aws-java-sdk-bundle-1.11.375.jar;
-      wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.2.0/hadoop-aws-3.2.0.jar -O spark-${SPARK_VERSION}-bin-hadoop3.2/jars/hadoop-aws-3.2.0.jar;
-  )
+aws_bundle_version="1.11.901"
+artifact="spark-${SPARK_VERSION}-bin-hadoop-${HADOOP_VERSION}"
+
+echo " ########## Building ${artifact} ..."
+
+if [ ! -d ${artifact} ]; then
+
+  echo " ########## Downloading spark ..."
+  git clone https://github.com/apache/spark.git spark_clone
+  cd spark_clone
+  git checkout "tags/v${SPARK_VERSION}" -b "v${SPARK_VERSION}"
+
+  echo " ########## Building spark ..."
+  ./dev/make-distribution.sh --name hadoop-${HADOOP_VERSION} --pip -Psparkr -Phive -Phive-thriftserver -Pkubernetes -Dhadoop.version="${HADOOP_VERSION}" -DskipTests
+  cd ..
+  mv spark_clone/dist ${artifact}
+
+  wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/${aws_bundle_version}/aws-java-sdk-bundle-${aws_bundle_version}.jar -O ${artifact}/jars/aws-java-sdk-bundle-${aws_bundle_version}.jar;
+  wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${HADOOP_VERSION}/hadoop-aws-${HADOOP_VERSION}.jar -O ${artifact}/jars/hadoop-aws-${HADOOP_VERSION}.jar;
+
+  # tar -cvzf ${artifact}.tgz ${artifact}
 fi
 
-(
-    cd work;
-    spark-${SPARK_VERSION}-bin-hadoop3.2/bin/docker-image-tool.sh -r ferlabcrsj -t ${SPARK_IMAGE_TAG} build;
-)
+
+./${artifact}/bin/docker-image-tool.sh -r ferlabcrsj -t ${SPARK_IMAGE_TAG} build;
 
 docker build --build-arg "SPARK_USER=185" --build-arg "SPARK_IMAGE_TAG=$SPARK_IMAGE_TAG" --build-arg "SPARK_IMAGE_REPO=ferlabcrsj" -t ferlabcrsj/spark:$SPARK_IMAGE_TAG .;
