443e053cf03899b7d4cd9883f4c72b638a61cc32
Issam <ihannache@ferlab.bio>
2023-11-13 11:34:07 -0500

    feat: INFRA-403 Optimize spark certs handling

--- client-entrypoint.sh
+++ client-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+# Run the certificate installation script
+/opt/install_ca.sh
+
+# Run the existing setup configuration script
 /opt/setup-config.sh;
 
+# Check for SPARK_JAR environment variable
 if [ -z "$SPARK_JAR" ]; then
     echo "SPARK_JAR environment variable needs to be defined";
     exit 1;
 fi
 
+# Submit the Spark job
 if [ ! -z "$SPARK_CLASS" ]; then
     /opt/spark/bin/spark-submit --class $SPARK_CLASS $SPARK_JAR "$@";
 else
